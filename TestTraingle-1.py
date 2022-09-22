7a5ecc74f036b1ccf82b3a38b5367c8e3ac01c04
Monicaprojects21 <monicamallampatti21@gmail.com>
2022-09-21 20:53:18 -0400

    sttributr error

--- TestTraingle-1.py
+++ TestTraingle-1.py
@@ -14,7 +14,7 @@ from Triangle import classifyTriangle
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
-class TestTriangles(pytest.TestCase):
+class TestTriangles(pytest.Test):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
